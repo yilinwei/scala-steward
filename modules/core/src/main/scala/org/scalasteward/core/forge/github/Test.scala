9efa8597d9e8891531666f8b9b4f0603f427c88a
Yilin Wei <yilin.wei@coralogix.com>
2025-07-17 15:17:48 +0100

    woops

---
+++ modules/core/src/main/scala/org/scalasteward/core/forge/github/Test.scala
@@ -0,0 +1,47 @@
+package org.scalasteward.core.forge.github
+
+import org.scalasteward.core.data.Repo
+import org.scalasteward.core.forge.data.PullRequestState
+
+import cats.syntax.all.*
+import cats.effect.*
+import org.http4s.syntax.all.*
+import org.http4s.jdkhttpclient.JdkHttpClient
+import org.http4s.client.Client
+import java.net.http.HttpClient
+import org.scalasteward.core.util.HttpJsonClient
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.typelevel.log4cats.Logger
+import org.http4s.*
+
+object GithubApiTest extends IOApp {
+
+  implicit private def client: Client[IO] = JdkHttpClient(HttpClient.newHttpClient())
+  implicit private def httpJsonClient: HttpJsonClient[IO] = new HttpJsonClient[IO]
+  implicit private def logger: Logger[IO] = Slf4jLogger.getLogger[IO]
+
+  def creds(token: String) = Credentials.Token(AuthScheme.Bearer, token)
+
+  private def auth(token: String)(req: Request[IO]): IO[Request[IO]] =
+    req.putHeaders(headers.Authorization(creds(token))).pure[IO]
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val api = new GitHubApiAlg[IO](uri"https://api.github.com", auth(args.head))
+    val repo = Repo("coralogix", "eng-svc-dataprime-api")
+    (for {
+      repoOut <- api.getRepo(repo)
+      prs <- api.listPullRequests(repo, repoOut.default_branch.name, repoOut.default_branch)
+      pr =
+        prs
+          .filter(_.state == PullRequestState.Closed)
+          .filter(_.title.startsWith("Update"))
+          .head
+      _ <- api.labelPullRequest(
+        repo,
+        pr.number,
+        List("dummy")
+      )
+
+    } yield println(pr)).as(ExitCode.Success)
+  }
+}
